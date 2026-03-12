6e2b3d8e0ee86c99e8713357ca8a47845fcb7a86
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-03-12 23:57:12 +0530

    Building Custom Exception Handler

--- multi_doc_chat/exception/custom_exception.py
+++ multi_doc_chat/exception/custom_exception.py
@@ -1,3 +1,13 @@
 import sys 
 import traceback
 from typing import Optional,cast
+
+class DocumentPortalException(Exception):
+    def __init__(self, error_message,error_details: Optional[object]=None):
+        
+        if isinstance(error_message,BaseException):
+            norm_msg = str(error_message)
+        else:
+            norm_msg = str(error_message)
+
+            
